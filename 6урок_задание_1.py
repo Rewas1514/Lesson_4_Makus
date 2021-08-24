08b011696685f84fe50be453c478bba941a2034b
Rewas1514 <88451739+Rewas1514@users.noreply.github.com>
2021-08-24 19:52:57 +0300

    Revert "Lesson_6"

--- 6урок_задание_1.py
+++
@@ -1,7 +0,0 @@
-with open('nginx_logs.txt') as c:
-    data = []
-    for line in c:
-        splitted = line.split()
-        data.append((splitted[0], splitted[5].replace('"',''),splitted[6]))
-
-print(data)
